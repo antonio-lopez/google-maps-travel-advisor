b6f56411b254b2216915b2f40201d0e4db4eb76d
Antonio Lopez <36496887+antonio-lopez@users.noreply.github.com>
2021-08-16 20:09:34 -0700

    displaying restaurants in a list
    added api key which displays restaurant information based on user location

--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -1,9 +1,33 @@
 import React from 'react';
+import GoogleMapReact from 'google-map-react';
+import { Paper, Typography, useMediaQuery } from '@material-ui/core';
+import LocationOnOutlinedIcon from '@material-ui/icons/LocationOnOutlined';
+import Rating from '@material-ui/lab';
 
-const Map = () => {
+import useStyles from './styles';
+
+const Map = ({ setCoordinates, setBounds, coordinates }) => {
+  const classes = useStyles();
+  const isMobile = useMediaQuery('(min-width:600px)'); //false if size > 600px
+
   return (
-    <div>
-      <h1>map</h1>
+    <div className={classes.mapContainer}>
+      <GoogleMapReact
+        bootstrapURLKeys={{
+          key: process.env.REACT_APP_GOOGLE_MAPS_API_KEY,
+        }}
+        defaultCenter={coordinates}
+        center={coordinates}
+        defaultZoom={14}
+        margin={[50, 50, 50, 50]}
+        options={''}
+        onChange={(e) => {
+          console.log(e);
+          setCoordinates({ lat: e.center.lat, lng: e.center.lng });
+          setBounds({ ne: e.marginBounds.ne, sw: e.marginBounds.sw });
+        }}
+        onChildClick={''}
+      ></GoogleMapReact>
     </div>
   );
 };
